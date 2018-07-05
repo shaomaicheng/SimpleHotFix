aa3d26a00c02e503ff69547fe73121e50a653e9d
chenglei <devchenglei@gmail.com>
2018-07-05 22:13:38 +0800

    add dex load and hot fix

---
+++ app/src/main/java/com/cl/BugTest.java
@@ -0,0 +1,16 @@
+package com.cl;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * create by chenglei at 2018/7/5
+ */
+public class BugTest {
+    public void getBug(Context context) {
+        // mock a bug
+        int i = 10;
+        int a = 0;
+        Toast.makeText(context, "Hello, Minuit: " + i / a, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/cl/MainActivity.java
+++ app/src/main/java/com/cl/MainActivity.java
@@ -2,12 +2,20 @@ package com.cl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnFix;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        new BugTest().getBug(MainActivity.this);
+    }
+
+    private void init() {
+        btnFix = findViewById(R.id.btnFix);
     }
 }
